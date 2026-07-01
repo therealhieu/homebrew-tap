bc4281b907b34bd123d0f9876030acf337fed36e
therealhieu <hieu.services001@gmail.com>
2026-07-01 18:18:12 +0700

    cc-profile 0.1.2

--- Formula/cc-profile.rb
+++ Formula/cc-profile.rb
@@ -1,20 +1,33 @@
 class CcProfile < Formula
   desc "Profile management for Claude Code endpoints and models"
   homepage "https://github.com/therealhieu/cc-profile"
-  url "https://github.com/therealhieu/cc-profile/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "eb4dc3926f024a6a5379151e98f5b722c4f9853806b4e3b46d5208ce1fd25887"
   license "MIT"
-  head "https://github.com/therealhieu/cc-profile.git", branch: "master"
 
   livecheck do
-    url :stable
+    url :homepage
     strategy :github_latest
   end
 
-  depends_on "rust" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/therealhieu/cc-profile/releases/download/v0.1.2/cc-profile-v0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "86df92297a7869f8ba2571c716da218ca8480cd0d252e0acf69cb29fdc98d0ea"
+    end
+    on_intel do
+      url "https://github.com/therealhieu/cc-profile/releases/download/v0.1.2/cc-profile-v0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "c6c30d8be3aa111126e65f6b9b13aeaa0360e326e42a72375b9b6a995d67c138"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/therealhieu/cc-profile/releases/download/v0.1.2/cc-profile-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "1254573e3c38e311f6d734661ce66b96266296f04999ae1bf8d5cb3cf09d3b70"
+    end
+  end
 
   def install
-    system "cargo", "install", "--locked", "--path", ".", "--root", prefix
+    bin.install "cc-profile"
   end
 
   test do
